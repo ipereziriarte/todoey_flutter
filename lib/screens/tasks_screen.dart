8b000021d63f2ccced3899f0484e0affb698c224
Imanol Pérez Iriarte <eltigrequehayenti@gmail.com>
2020-11-09 20:01:29 +0100

    Handle state with the provider package
    See https://www.youtube.com/watch?v=d_m5csmrf7

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
 import 'package:todoey_flutter/widgets/tasks_list.dart';
 
 import 'add_task_screen.dart';
@@ -28,7 +30,7 @@ class TasksScreen extends StatelessWidget {
         children: <Widget>[
           Container(
             padding: EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-            child: Column(children: <Widget>[
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
               CircleAvatar(
                 backgroundColor: Colors.white,
                 radius: 30.0,
@@ -46,7 +48,7 @@ class TasksScreen extends StatelessWidget {
                 ),
               ),
               Text(
-                '12 tasks',
+                '${Provider.of<TaskData>(context).taskCount} tasks',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 18.0,
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
 class TasksList extends StatelessWidget {
